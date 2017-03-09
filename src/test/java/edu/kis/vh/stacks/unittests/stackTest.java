442fd859b3bae0072869cce09f9dff9471e482c7
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-09 16:03:16 +0100

    Podpunkt 1 z zadania 3.2 - dodanie testów jednostkowych

--- src/test/java/edu/kis/vh/stacks/unittests/stackTest.java
+++ src/test/java/edu/kis/vh/stacks/unittests/stackTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.StackHanoi;
+import edu.kis.vh.stacks.factory.DefaultStacksFactory;
 
 public class stackTest {
 	
@@ -77,4 +79,48 @@ public class stackTest {
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
 	}
 
+	@Test
+	public void testFIFOPop() {
+		DefaultStacksFactory factory = new DefaultStacksFactory();
+		Stack stackObj = factory.getFIFOStack();
+		final int EMPTY_STACK_VALUE = -1;
+		
+		int result = stackObj.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+		
+		int testValue = 4;
+		stackObj.push(testValue);
+		
+		result = stackObj.pop();
+		Assert.assertEquals(testValue, result);
+		result = stackObj.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+	}
+	
+	@Test
+	public void testHanoiPush() {
+		DefaultStacksFactory factory = new DefaultStacksFactory();
+		Stack stackObj = factory.getHanoiStack();
+		int testValue = 4;
+		stackObj.push(testValue);
+		
+		int result = stackObj.top();
+		Assert.assertEquals(testValue, result);
+	}
+	
+	@Test
+	public void testReportRejected() {
+		StackHanoi stackObj = new StackHanoi();
+		final int REPORTED_REJECTED = 2;
+		
+		int testValue1 = 1;
+		stackObj.push(testValue1);
+		int testValue2 = 2;
+		stackObj.push(testValue2);
+		int testValue3 = 3;
+		stackObj.push(testValue3);
+		
+		int result = stackObj.reportRejected();
+		Assert.assertEquals(REPORTED_REJECTED, result);
+	}
 }
