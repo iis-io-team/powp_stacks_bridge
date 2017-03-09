469b85204d34d05ef222e1ac5f2e583d65cfa5ca
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-09 16:35:52 +0100

    Podpunkt 2 z zadania 3.3 - szkielet dokumentacji dla klasy stackFIFO

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -1,13 +1,24 @@
 package edu.kis.vh.stacks;
 
+/**
+ * Jest to klasa dla stosu FIFO.
+ * @author Jakub
+ *
+ */
 public class StackFIFO extends Stack {
 
 	private Stack temp = new Stack(); //po enkapsulacji dodanie gettera i settera po enkapsulacji
 	
+	/**
+	 * @return tymczasowa wartość temp
+	 */
 	public Stack getTemp() {
 		return temp;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.Stack#pop()
+	 */
 	@Override
 	public int pop() {
 		while (!isEmpty())
