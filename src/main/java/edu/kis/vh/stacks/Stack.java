db202bee5c34e428e4e4eb2dfbf38a8f8c41035b
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-09 14:55:51 +0100

    Podpunkt 7 z zadania 3.1 - generacja gettera dla total

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -9,6 +9,10 @@ public class Stack {
 
 	private int total = EMPTY_STACK_INDICATOR;
 	
+	public int getTotal() {
+		return total;
+	}
+
 	public void push(int i) {
 		if (!isFull())
 			ITEMS[++total] = i;
