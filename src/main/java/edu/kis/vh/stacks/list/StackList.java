42ddab9de08a1bfd3d7cca7f09ce1f2c932e71ab
Michal Kaminski <michalkam12@gmail.com>
2017-03-20 20:44:21 +0100

    class moved

--- src/main/java/edu/kis/vh/stacks/list/StackList.java
+++ src/main/java/edu/kis/vh/stacks/list/StackList.java
@@ -37,3 +37,39 @@ public class StackList {
 	}
 
 }
+
+public class Node {
+
+	private int value;
+	private Node prev;
+	private Node next;
+
+	Node(int i) {
+		setValue(i);
+	}
+
+	public int getValue() {
+ 		return value;
+ 	}
+
+ 	public void setValue(int value) {
+ 		this.value = value;
+ 	}
+
+ 	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+  	}
+
+}
