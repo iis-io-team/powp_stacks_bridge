792d24eb394dfc0a2af562a2b9227d9acf468ed8
Michal Kaminski <michalkam12@gmail.com>
2017-03-20 21:56:04 +0100

    documentation

--- src/main/java/edu/kis/vh/stacks/stack.java
+++ src/main/java/edu/kis/vh/stacks/stack.java
@@ -7,31 +7,53 @@ public class stack {
 
 	private int total = emptyStack;
 
+
+	/**
+	 * Ta metoda dodaje nowe elementy do stosu
+	 * @param i to element ktory bedzie dodany
+	 */
 	public void push(int i) {
 		if (!isFull())
 		ITEMS[++total] = i;
 	}
 
+	/**
+	 * Ta metoda sprawdza czy stos jest pusty
+	 * @return zwraca prawde jesli stos jest pusty
+	 */
 	public boolean isEmpty() {
 		return total == emptyStack;
 	}
 
+	/**
+	 * Ta metoda sprawdza czy stos jest pelen
+	 * @return zwraca prawde jezeli stos jest pelen
+	 */
 	public boolean isFull() {
 		return total == 11;
 	}
 
+	/**
+	 * Ta metoda zwraca wartosc z gory stosu
+	 */
 	public int top() {
 		if (isEmpty())
 			return emptyStack;
 		return ITEMS[total];
 	}
 
+	/**
+	 * Ta metoda zwraca wartosc z gory stosu oraz ja usuwa
+	 */
 	public int pop() {
 		if (isEmpty())
 			return emptyStack;
 		return ITEMS[total--];
 	}
 
+	/**
+	 * Ta metoda to getter dla wartosci total
+	 */
 	public int getTotal() {
 		return total;
 	}
