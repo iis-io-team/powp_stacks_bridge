0c2d824bdf60e934293361ca1fa006ed11079b90
Michal Kaminski <michalkam12@gmail.com>
2017-03-20 20:28:00 +0100

    getter added

--- src/main/java/edu/kis/vh/stacks/stack.java
+++ src/main/java/edu/kis/vh/stacks/stack.java
@@ -3,33 +3,38 @@ package edu.kis.vh.stacks;
 public class stack {
 	private final int size = 12;
 	private final int emptyStack = -1;
-	private int[] ITEMS = new int[size];	
+	private int[] ITEMS = new int[size];
 
 	private int total = emptyStack;
-	
+
 	public void push(int i) {
 		if (!isFull())
 		ITEMS[++total] = i;
 	}
-	
-		public boolean isEmpty() {
-			return total == emptyStack;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-			
-				public int top() {
-					if (isEmpty())
-						return emptyStack;
-					return ITEMS[total];
-				}
-				
-					public int pop() {
-						if (isEmpty())
-							return emptyStack;
-						return ITEMS[total--];
-					}
-				
+
+	public boolean isEmpty() {
+		return total == emptyStack;
+	}
+
+	public boolean isFull() {
+		return total == 11;
+	}
+
+	public int top() {
+		if (isEmpty())
+			return emptyStack;
+		return ITEMS[total];
+	}
+
+	public int pop() {
+		if (isEmpty())
+			return emptyStack;
+		return ITEMS[total--];
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+
 }
