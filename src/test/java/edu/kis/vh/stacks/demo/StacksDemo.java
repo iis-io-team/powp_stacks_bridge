5f5deec623afeaf3f75fe7e75fdaf4e9b0840e4e
Sebastian Zielinski <195100@edu.p.lodz.pl>
2017-03-22 23:09:48 +0100

    ex 3.1.1 3.1.2 3.1.2

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -8,27 +8,28 @@ class StacksDemo {
 
 	public static void main(String[] args) {
 		DefaultStacksFactory factory = new DefaultStacksFactory();
-		
-		stack[] stacks = { factory.GetStandardStack(), factory.GetFalseStack(),
-				factory.GetFIFOStack(), factory.GetHanoiStack()};
-		
+
+		stack[] stacks = { factory.GetStandardStack(), factory.GetFalseStack(), factory.GetFIFOStack(),
+				factory.GetHanoiStack() };
+
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				stacks[j].push(i);
-			
-			java.util.Random rn = new java.util.Random();
-			for (int i = 1; i < 15; i++)
-				stacks[3].push(rn.nextInt(20));
-			
-			for (int i = 0; i < stacks.length; i++) {
-				while (!stacks[i].isEmpty())
-					System.out.print(stacks[i].pop() + "  ");
-					System.out.println();
-			}
-		
-		System.out.println("total rejected is "
-				+ ((StackHanoi) stacks[3]).reportRejected());
-		
+
+		java.util.Random rn = new java.util.Random();
+		for (int i = 1; i < 15; i++)
+			stacks[3].push(rn.nextInt(20));
+
+		for (int i = 0; i < stacks.length; i++) {
+			while (!stacks[i].isEmpty())
+				System.out.print(stacks[i].pop() + "  ");
+			System.out.println();
+		}
+
+		System.out.println("total rejected is " + ((StackHanoi) stacks[3]).reportRejected());
+
 	}
-	
+//	13,19-27 to big intend
+//	alt + arrow move between files
+
 }
--- src/test/java/edu/kis/vh/stacks/unittests/stackTest.java
+++ src/test/java/edu/kis/vh/stacks/unittests/stackTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 import edu.kis.vh.stacks.stack;
 
 public class stackTest {
-	
+
 	@Test
 	public void testPush() {
 		stack stackObj = new stack();
 		int testValue = 4;
 		stackObj.push(testValue);
-		
+
 		int result = stackObj.top();
 		Assert.assertEquals(testValue, result);
 	}
@@ -20,11 +20,11 @@ public class stackTest {
 	@Test
 	public void testIsEmpty() {
 		stack stackObj = new stack();
-		boolean result = stackObj.isEmpty();		
+		boolean result = stackObj.isEmpty();
 		Assert.assertEquals(true, result);
-		
+
 		stackObj.push(888);
-		
+
 		result = stackObj.isEmpty();
 		Assert.assertEquals(false, result);
 	}
@@ -34,11 +34,11 @@ public class stackTest {
 		stack stackObj = new stack();
 		final int STACK_CAPACITY = 12;
 		for (int i = 0; i < STACK_CAPACITY; i++) {
-			boolean result = stackObj.isFull();		
+			boolean result = stackObj.isFull();
 			Assert.assertEquals(false, result);
 			stackObj.push(888);
 		}
-		
+
 		boolean result = stackObj.isFull();
 		Assert.assertEquals(true, result);
 	}
@@ -47,13 +47,13 @@ public class stackTest {
 	public void testTop() {
 		stack stackObj = new stack();
 		final int EMPTY_STACK_VALUE = -1;
-		
+
 		int result = stackObj.top();
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
-		
+
 		int testValue = 4;
 		stackObj.push(testValue);
-		
+
 		result = stackObj.top();
 		Assert.assertEquals(testValue, result);
 		result = stackObj.top();
@@ -64,13 +64,13 @@ public class stackTest {
 	public void testPop() {
 		stack stackObj = new stack();
 		final int EMPTY_STACK_VALUE = -1;
-		
+
 		int result = stackObj.pop();
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
-		
+
 		int testValue = 4;
 		stackObj.push(testValue);
-		
+
 		result = stackObj.pop();
 		Assert.assertEquals(testValue, result);
 		result = stackObj.pop();
