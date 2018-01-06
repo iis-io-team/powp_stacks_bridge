8ba05325309d54ffaf4e0a22f0a73cc83a6d51dc
Rafal Kosinski <fanghon@gmail.com>
2018-01-06 11:14:03 +0100

    Java doc added

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -1,46 +1,85 @@
 package edu.kis.vh.stacks;
 
+/**
+ * Represents the default Stack.
+ * 
+ * @author rafal
+ *
+ */
 public class Stack {
 
-	private static final int EMPTY_STACK_VALUE = -1;
+	/** The empty stack value. */
+	public static final int EMPTY_STACK_VALUE = -1;
 
+	/** The elements in stack. */
 	private final int[] items;
 
+	/** The total number of elements in stack. */
 	private int total = EMPTY_STACK_VALUE;
 
+	/** The stacks capacity. */
 	private final int capacity;
 
+	/**
+	 * Creates new instance of Stack.
+	 * 
+	 * @param capacity
+	 *            The max capacity of this stack.
+	 */
 	public Stack(int capacity) {
 		this.capacity = capacity;
 		this.items = new int[capacity];
 		;
 	}
 
+	/**
+	 * Adds new value to stack.
+	 * 
+	 * @param i
+	 *            The value to add.
+	 */
 	public void push(int i) {
 		if (!isFull())
 			items[++total] = i;
 	}
 
+	/**
+	 * @return <code>true</code> if stack is empty.
+	 */
 	public boolean isEmpty() {
 		return total == EMPTY_STACK_VALUE;
 	}
 
+	/**
+	 * @return <code>true</code> if total number of items reached stack capacity.
+	 */
 	public boolean isFull() {
 		return total == capacity - 1;
 	}
 
+	/**
+	 * @return The last value in stack (or {@link EMPTY_STACK_VALUE} if empty).
+	 */
 	public int top() {
 		if (isEmpty())
 			return EMPTY_STACK_VALUE;
 		return items[total];
 	}
 
+	/**
+	 * 
+	 * @return The last value and remove it from stack.
+	 */
 	public int pop() {
 		if (isEmpty())
 			return EMPTY_STACK_VALUE;
 		return items[total--];
 	}
 
+	/**
+	 * 
+	 * @return The total number of items in stack.
+	 */
 	public int getTotal() {
 		return total;
 	}
